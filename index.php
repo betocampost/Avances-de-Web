1fdb6b11e4924dc92bc0ffa7340ccf6c0a250410
Hernan51 <119095998+Hernan51@users.noreply.github.com>
2022-12-10 18:31:13 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -38,6 +38,7 @@
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                   <li class="nav-item"><a class="nav-link active" aria-current="page" href="index.php">Inicio</a></li>
+                  <li class="nav-item"><a class="nav-link" href="admin/tienda.php">Tienda</a></li>
                   <li class="nav-item"><a class="nav-link" href="acercaDe.php">Acerca de</a></li>
                   <li class="nav-item"><a class="nav-link" href="ayuda.php">Ayuda</a></li>
                   <li class="nav-item"><a class="nav-link" href="contacto.php">Contáctanos</a></li>
